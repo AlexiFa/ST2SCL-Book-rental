f2412f00b22a3eb9abe8fb487fb20a389fa0d9be
Alexis Faure <113013323+AlexiFa@users.noreply.github.com>
2025-03-02 12:44:53 +0100

    user service connected to books service

--- userService/src/main/java/com/example/userService/HelloService.java
+++ userService/src/main/java/com/example/userService/HelloService.java
@@ -1,11 +1,23 @@
 package com.example.userService;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloService {
-    @GetMapping("/hello")
+
+    @Value("${booksServiceUrl}")
+    String booksServiceUrl;
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Hello, World! from user service";
+        try {
+            RestTemplate restTemplate = new RestTemplate();
+            String messageFromBooksService = restTemplate.getForObject(booksServiceUrl + "/hello", String.class);
+            return "Hello, World! from user service and this is the message from the book service : " + messageFromBooksService;
+        } catch (Exception e) {
+            return e.getLocalizedMessage();
+        }
     }
 }
